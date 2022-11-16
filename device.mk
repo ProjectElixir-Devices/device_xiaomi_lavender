9f4fbc79e44a2b41e71d76713c63e27d01c20411
TogoFire <italomellopereira@gmail.com>
2022-11-16 21:25:04 +0530

    lavender:  build libqdMetaData from source
    Signed-off-by: OmarAlCoptan <omarcoptan9@gmail.com>
    Change-Id: e1273fa4806c84856e318b880a187861bdea1cb8

--- device.mk
+++ device.mk
@@ -165,7 +165,9 @@ PRODUCT_PACKAGES += \
     hwcomposer.sdm660 \
     memtrack.sdm660 \
     libdisplayconfig \
-    libtinyxml
+    libtinyxml \
+    libqdMetaData \
+    libqdMetaData.vendor
 
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
