09d0930c978377627dba7d8547d7c08274088be9
OmarAlCoptan <omarcoptan9@gmail.com>
2022-12-02 17:46:00 +0200

    lavender: it's EvolutionX time!
    Signed-off-by: OmarAlCoptan <omarcoptan9@gmail.com>

--- evolution_lavender.mk
+++ evolution_lavender.mk
@@ -8,19 +8,21 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
-
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-PRODUCT_BRAND := Xiaomi
-PRODUCT_DEVICE := lavender
-PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := arrow_lavender
-PRODUCT_MODEL := Redmi Note 7
+# Inherit some common Evolution X stuff
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+EVO_BUILD_TYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
 
-DEVICE_MAINTAINER := Ratoriku, Hazama25
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := evolution_lavender
+PRODUCT_DEVICE := lavender
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := Redmi Note 7
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
